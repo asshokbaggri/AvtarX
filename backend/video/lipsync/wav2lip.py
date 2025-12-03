6e3c172a58510742f8369846a9f74dd567b61f35
asshokbaggri <asshokbaggri@gmail.com>
2025-12-03 15:34:22 +0530

    Create wav2lip.py

---
+++ backend/video/lipsync/wav2lip.py
@@ -0,0 +1,10 @@
+def generate_lipsync(motion_video, audio):
+    """
+    REAL VERSION:
+    - Load Wav2Lip model
+    - Generate perfect lipsync
+
+    CURRENT: return same video (dummy)
+    """
+
+    return motion_video
