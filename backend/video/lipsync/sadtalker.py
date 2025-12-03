17186862d443f22cb611eab0939a379dc7bb4eb0
asshokbaggri <asshokbaggri@gmail.com>
2025-12-03 15:33:49 +0530

    Create sadtalker.py

---
+++ backend/video/lipsync/sadtalker.py
@@ -0,0 +1,11 @@
+def generate_motion(avatar_url, audio_url):
+    """
+    REAL VERSION:
+    - Download avatar & audio
+    - Run SADTalker inference
+    - Generate motion video
+
+    CURRENT: return dummy file
+    """
+
+    return "/tmp/dummy_motion.mp4"
