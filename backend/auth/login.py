8361fc9bb8ff369dcf5e419851dae59814de0c98
asshokbaggri <asshokbaggri@gmail.com>
2025-12-03 14:29:47 +0530

    Update login.py

--- backend/auth/login.py
+++ backend/auth/login.py
@@ -1,2 +1,14 @@
+from firebase_admin import auth
+from auth.firebase_init import get_auth
+
 def login_user(data):
-    return {"message": "Login successful", "data": data}
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return {"error": "Email & Password required"}
+
+    # Firebase does NOT support password login on backend
+    # So user will login through Flutter & send ID token here
+
+    return {"message": "Use /verify to validate ID token"}
